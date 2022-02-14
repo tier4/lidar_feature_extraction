94e833cecba8eb677e50f33a881a42fae6d19360
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-02-14 23:21:23 +0900

    Make Convolution1D throw std::invalid_argument if weight is larger than input

--- include/utility.hpp
+++ include/utility.hpp
@@ -263,6 +263,14 @@ std::vector<double> Convolution1D(
 {
   const int input_size = input_end - input_begin;
   const int weight_size = weight_end - weight_begin;
+
+  if (input_size < weight_size) {
+    auto s = fmt::format(
+      "Input array size {} cannot be smaller than weight size {}",
+      input_size, weight_size);
+    throw std::invalid_argument(s);
+  }
+
   std::vector<double> result(input_size - weight_size + 1);
   for (unsigned int i = 0; i < result.size(); i++) {
     auto iter = input_begin + i;
--- test/test_utility.cpp
+++ test/test_utility.cpp
@@ -116,11 +116,34 @@ TEST(Utility, IndexRange) {
 }
 
 TEST(Utility, Convolution1D) {
-  std::vector<double> input{1., -1, 2., 0., 1};
-  std::vector<double> weight{1., 0, -1};
+  {
+    std::vector<double> input{1., -1, 2., 0., 1};
+    std::vector<double> weight{1., 0., -1};
 
-  const auto result = Convolution1D(input.begin(), input.end(), weight.begin(), weight.end());
-  EXPECT_THAT(result, testing::ElementsAre(-1., -1., 1.));
+    const auto result = Convolution1D(input.begin(), input.end(), weight.begin(), weight.end());
+    EXPECT_THAT(result, testing::ElementsAre(-1., -1., 1.));
+  }
+
+  {
+    std::vector<double> input{1., -1, 2.};
+    std::vector<double> weight{1., 0., -1};
+
+    const auto result = Convolution1D(input.begin(), input.end(), weight.begin(), weight.end());
+    EXPECT_THAT(result, testing::ElementsAre(-1.));
+  }
+
+  {
+    std::vector<double> input{2., 0.};
+    std::vector<double> weight{1., 0., -1};
+    EXPECT_THROW(
+      try {
+        Convolution1D(input.begin(), input.end(), weight.begin(), weight.end());
+      } catch (const std::invalid_argument & e) {
+        EXPECT_STREQ("Input array size 2 cannot be smaller than weight size 3", e.what());
+        throw e;
+      },
+      std::invalid_argument);
+  }
 }
 
 TEST(Utility, FilterByRange) {
