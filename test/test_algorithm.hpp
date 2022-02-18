eba9c1165621c1a86a10aa83ca35b87e8ef57ac1
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-02-19 03:59:50 +0900

    Move argsort test to 'test_algorithm.hpp'

---
+++ test/test_algorithm.hpp
@@ -0,0 +1,25 @@
+// BSD 3-Clause License
+// Copyright (c) 2020, Tixiao Shan, Takeshi Ishita
+// All rights reserved.
+
+#include <gmock/gmock.h>
+
+#include <vector>
+
+#include "algorithm.hpp"
+
+
+TEST(Utility, Argsort)
+{
+  {
+    const std::vector<double> curvature{0.3, 0.2, 1.0, 0.2, 0.0, 0.1};
+    const std::vector<int> indices = Argsort(curvature);
+    EXPECT_THAT(indices, testing::ElementsAre(4, 5, 1, 3, 0, 2));
+  }
+
+  {
+    const std::vector<double> curvature{0.0, 0.0, 0.0, 0.0, 0.0};
+    const std::vector<int> indices = Argsort(curvature);
+    EXPECT_THAT(indices, testing::ElementsAre(0, 1, 2, 3, 4));
+  }
+}
--- test/test_utility.cpp
+++ test/test_utility.cpp
@@ -127,21 +127,6 @@ TEST(Utility, ExtractSectionsByRing) {
   }
 }
 
-TEST(Utility, Argsort)
-{
-  {
-    const std::vector<double> curvature{0.3, 0.2, 1.0, 0.2, 0.0, 0.1};
-    const std::vector<int> indices = Argsort(curvature);
-    EXPECT_THAT(indices, testing::ElementsAre(4, 5, 1, 3, 0, 2));
-  }
-
-  {
-    const std::vector<double> curvature{0.0, 0.0, 0.0, 0.0, 0.0};
-    const std::vector<int> indices = Argsort(curvature);
-    EXPECT_THAT(indices, testing::ElementsAre(0, 1, 2, 3, 4));
-  }
-}
-
 TEST(Utility, Convolution1D) {
   {
     std::vector<double> input{1., -1, 2., 0., 1};
