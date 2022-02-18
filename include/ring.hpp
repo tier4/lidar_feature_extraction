e8d39a7ba268c2314cbb7db316ffdf29d81282cd
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-02-19 01:02:54 +0900

    Add a function to sort each ring by its atan2 value

--- include/ring.hpp
+++ include/ring.hpp
@@ -18,21 +18,57 @@
 
 #include "cloud_iterator.hpp"
 
-template<typename PointT>
-using ReferenceVector = std::vector<std::reference_wrapper<PointT>>;
+template<typename Iterator>
+using ElementType = typename std::iterator_traits<typename Iterator::iterator>::value_type;
 
-template<typename Iter>
-void SortByAtan2(Iter & iter)
+template<typename Element>
+using ConstReferenceVector = std::vector<std::reference_wrapper<const Element>>;
+
+template<typename Element>
+void SortByAtan2(ConstReferenceVector<Element> & iter)
 {
-  typedef typename std::iterator_traits<typename Iter::iterator>::value_type Element;
-
-  auto f = [](const Element & p1, const Element & p2) {
+  auto f = [](
+    const std::reference_wrapper<const Element> & ref_p1,
+    const std::reference_wrapper<const Element> & ref_p2) {
+      const Element & p1 = ref_p1.get();
+      const Element & p2 = ref_p2.get();
       const double angle1 = std::atan2(p1.y, p1.x);
       const double angle2 = std::atan2(p2.y, p2.x);
       return angle1 < angle2;
     };
 
   std::sort(iter.begin(), iter.end(), f);
+}
+
+template<typename Iter>
+std::unordered_map<int, ConstReferenceVector<ElementType<Iter>>>
+MakeReferenceVectorsPerRing(const Iter & points)
+{
+  typedef ElementType<Iter> Element;
+
+  std::unordered_map<int, ConstReferenceVector<Element>> rings;
+
+  for (const Element & p : points) {
+    rings[p.ring].push_back(std::cref(p));
+  }
+  return rings;
+}
+
+template<typename Element>
+void SortEachRingByAngle(std::unordered_map<int, ConstReferenceVector<Element>> & rings)
+{
+  for (auto & [ring, points] : rings) {
+    SortByAtan2<Element>(points);
+  }
+}
+
+template<typename Iter>
+std::unordered_map<int, ConstReferenceVector<ElementType<Iter>>>
+ExtractAngleSortedRings(const Iter & iterator)
+{
+  auto rings = MakeReferenceVectorsPerRing(iterator);
+  SortEachRingByAngle<ElementType<Iter>>(rings);
+  return rings;
 }
 
 template<typename PointT>
--- test/test_ring.cpp
+++ test/test_ring.cpp
@@ -2,19 +2,17 @@
 // Copyright (c) 2020, Tixiao Shan, Takeshi Ishita
 // All rights reserved.
 
+#include <iterator>
+
 #include <gmock/gmock.h>
 
 #include "ring.hpp"
 
-TEST(Ring, AngleFromX1Y0)
+TEST(Ring, SortByAtan2)
 {
   struct Point {
     double x;
     double y;
-  };
-
-  auto to_vector = [](const Point & p) {
-    return std::vector<double>{p.x, p.y};
   };
 
   std::vector<Point> points;
@@ -25,12 +23,58 @@ TEST(Ring, AngleFromX1Y0)
   points.push_back(Point{0., -1.});
   points.push_back(Point{-1., -1.});
 
-  SortByAtan2(points);
+  std::vector<std::reference_wrapper<const Point>> point_refs(points.begin(), points.end());
+  SortByAtan2<Point>(point_refs);
 
-  EXPECT_THAT(to_vector(points.at(0)), testing::ElementsAre(-1., -1.));
-  EXPECT_THAT(to_vector(points.at(1)), testing::ElementsAre(0., -1.));
-  EXPECT_THAT(to_vector(points.at(2)), testing::ElementsAre(1., -1.));
-  EXPECT_THAT(to_vector(points.at(3)), testing::ElementsAre(1., 0.));
-  EXPECT_THAT(to_vector(points.at(4)), testing::ElementsAre(1., 1.));
-  EXPECT_THAT(to_vector(points.at(5)), testing::ElementsAre(0., 1.));
+  auto to_vector = [](const std::reference_wrapper<const Point> & ref_p) {
+    const Point & p = ref_p.get();
+    return std::vector<double>{p.x, p.y};
+  };
+
+  EXPECT_THAT(to_vector(point_refs.at(0)), testing::ElementsAre(-1., -1.));
+  EXPECT_THAT(to_vector(point_refs.at(1)), testing::ElementsAre(0., -1.));
+  EXPECT_THAT(to_vector(point_refs.at(2)), testing::ElementsAre(1., -1.));
+  EXPECT_THAT(to_vector(point_refs.at(3)), testing::ElementsAre(1., 0.));
+  EXPECT_THAT(to_vector(point_refs.at(4)), testing::ElementsAre(1., 1.));
+  EXPECT_THAT(to_vector(point_refs.at(5)), testing::ElementsAre(0., 1.));
+}
+
+TEST(Ring, ExtractAngleSortedRings) {
+  struct PointWithRing {
+    int ring;
+    double x;
+    double y;
+  };
+
+  std::vector<PointWithRing> points;
+  points.push_back(PointWithRing{0,  1.,  1.});
+  points.push_back(PointWithRing{0,  1.,  0.});
+
+  points.push_back(PointWithRing{1,  1., -1.});
+  points.push_back(PointWithRing{1,  1.,  0.});
+  points.push_back(PointWithRing{1,  0.,  1.});
+
+  points.push_back(PointWithRing{2,  1.,  1.});
+  points.push_back(PointWithRing{2,  0., -1.});
+  points.push_back(PointWithRing{2, -1., -1.});
+
+  auto get_xy = [](const PointWithRing & p) {
+    return std::vector<double>{p.x, p.y};
+  };
+
+  const auto rings = ExtractAngleSortedRings(points);
+
+  const auto ring0 = rings.at(0);
+  EXPECT_THAT(get_xy(ring0.at(0)), testing::ElementsAre(1., 0.));
+  EXPECT_THAT(get_xy(ring0.at(1)), testing::ElementsAre(1., 1.));
+
+  const auto ring1 = rings.at(1);
+  EXPECT_THAT(get_xy(ring1.at(0)), testing::ElementsAre(1., -1.));
+  EXPECT_THAT(get_xy(ring1.at(1)), testing::ElementsAre(1.,  0.));
+  EXPECT_THAT(get_xy(ring1.at(2)), testing::ElementsAre(0.,  1.));
+
+  const auto ring2 = rings.at(2);
+  EXPECT_THAT(get_xy(ring2.at(0)), testing::ElementsAre(-1.,  -1.));
+  EXPECT_THAT(get_xy(ring2.at(1)), testing::ElementsAre( 0.,  -1.));
+  EXPECT_THAT(get_xy(ring2.at(2)), testing::ElementsAre( 1.,   1.));
 }
