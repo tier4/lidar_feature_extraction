8c34f4d8a8cef8612966d837145d08b032233fe9
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-03-04 18:32:23 +0900

    Fix the copyright year

--- extraction/include/lidar_feature_extraction/index_range.hpp
+++ extraction/include/lidar_feature_extraction/index_range.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Tixiao Shan, Takeshi Ishita (2020)
+// Copyright 2022 Tixiao Shan, Takeshi Ishita
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -10,7 +10,7 @@
 //      notice, this list of conditions and the following disclaimer in the
 //      documentation and/or other materials provided with the distribution.
 //
-//    * Neither the name of the Tixiao Shan, Takeshi Ishita (2020) nor the names of its
+//    * Neither the name of the Tixiao Shan, Takeshi Ishita nor the names of its
 //      contributors may be used to endorse or promote products derived from
 //      this software without specific prior written permission.
 //
