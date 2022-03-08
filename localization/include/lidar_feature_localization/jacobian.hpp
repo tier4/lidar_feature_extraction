1ed0c9e288fca3f7cc8516bef1c4f73cc7026884
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-03-08 13:06:26 +0900

    Add a jacobian calculator

---
+++ localization/include/lidar_feature_localization/jacobian.hpp
@@ -0,0 +1,62 @@
+// Copyright 2022 Tixiao Shan, Takeshi Ishita
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Tixiao Shan, Takeshi Ishita nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef JACOBIAN_HPP_
+#define JACOBIAN_HPP_
+
+#include "rotationlib/jacobian/quaternion.hpp"
+
+#include <Eigen/Core>
+
+#include <vector>
+
+void FillJacobianRow(
+  Eigen::MatrixXd & J,
+  const int i,
+  const Eigen::Matrix<double, 3, 4> & drpdq,
+  const Eigen::Vector3d & coeff)
+{
+  J.block<1, 4>(i, 0) = coeff.transpose() * drpdq;
+  J.block<1, 3>(i, 4) = coeff;
+}
+
+Eigen::MatrixXd MakeJacobian(
+  const std::vector<Eigen::Vector3d> & points,
+  const std::vector<Eigen::Vector3d> & coeffs,
+  const Eigen::Quaterniond & q)
+{
+  assert(points.size() == coeffs.size());
+  Eigen::MatrixXd J(points.size(), 6);
+  for (unsigned int i = 0; i < points.size(); i++) {
+    const Eigen::Matrix<double, 3, 4> drpdq = rotationlib::DRpDq(q, points.at(i));
+    FillJacobianRow(J, i, drpdq, coeffs.at(i));
+  }
+  return J;
+}
+
+#endif  // JACOBIAN_HPP_
