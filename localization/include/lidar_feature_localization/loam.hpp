a6a806fb97922ca832fe75ba7d6bcacc4c03548d
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-03-17 16:36:13 +0900

    Move functions related to LOAM optimization to 'loam.hpp'

--- localization/include/lidar_feature_localization/loam.hpp
+++ localization/include/lidar_feature_localization/loam.hpp
@@ -48,9 +48,116 @@
 #include "lidar_feature_localization/math.hpp"
 
 
+template<typename Iter>
+auto Filter(
+  const std::vector<bool> & flags,
+  const Iter & values)
+{
+  typedef typename Iter::value_type T;
+
+  assert(flags.size() == values.size());
+  std::vector<T> filtered;
+  for (unsigned int i = 0; i < flags.size(); i++) {
+    if (flags[i]) {
+      filtered.push_back(values[i]);
+    }
+  }
+  return filtered;
+}
+
+Eigen::Vector3d GetXYZ(const pcl::PointXYZ & point)
+{
+  return Eigen::Vector3d(point.x, point.y, point.z);
+}
+
+pcl::PointXYZ MakePointXYZ(const Eigen::Vector3d & v)
+{
+  return pcl::PointXYZ(v(0), v(1), v(2));
+}
+
+Eigen::MatrixXd Get(
+  const pcl::PointCloud<pcl::PointXYZ>::Ptr & pointcloud,
+  const std::vector<int> & indices)
+{
+  Eigen::MatrixXd A(indices.size(), 3);
+  for (const auto & [j, index] : ranges::views::enumerate(indices)) {
+    A.row(j) = GetXYZ(pointcloud->at(index)).transpose();
+  }
+  return A;
+}
+
+Eigen::Matrix3d CalcCovariance(const Eigen::MatrixXd & X)
+{
+  const Eigen::Vector3d c = X.colwise().mean();
+  const Eigen::MatrixXd D = X.rowwise() - c.transpose();
+  return D.transpose() * D / X.rows();
+}
+
+
+double PointPlaneDistance(const Eigen::Vector3d & w, const Eigen::Vector3d & x)
+{
+  return std::abs(w.dot(x) + 1.0) / w.norm();
+}
+
+bool ValidatePlane(const Eigen::MatrixXd & X, const Eigen::Vector3d & w)
+{
+  for (int j = 0; j < X.rows(); j++) {
+    const Eigen::Vector3d x = X.row(j);
+    if (PointPlaneDistance(w, x) > 0.2) {
+      return false;
+    }
+  }
+  return true;
+}
+
+Eigen::Vector3d TripletCross(
+  const Eigen::Vector3d & p0,
+  const Eigen::Vector3d & p1,
+  const Eigen::Vector3d & p2)
+{
+  return (p2 - p1).cross((p0 - p1).cross(p0 - p2));
+}
+
+Eigen::Vector3d EdgeCoefficient(
+  const Eigen::Vector3d p0,
+  const Eigen::Vector3d & center,
+  const Eigen::Vector3d & eigenvector)
+{
+  const Eigen::Vector3d p1 = center + 0.1 * eigenvector;
+  const Eigen::Vector3d p2 = center - 0.1 * eigenvector;
+  return TripletCross(p0, p1, p2);
+}
+
+std::tuple<Eigen::Vector3d, Eigen::Matrix3d> PrincipalComponents(const Eigen::Matrix3d & C)
+{
+  const Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> solver(C);
+  return {solver.eigenvalues(), solver.eigenvectors()};
+}
+
+template<int N>
+Eigen::Vector3d Center(const Eigen::Matrix<double, N, 3> & neighbors)
+{
+  return neighbors.colwise().mean();
+}
+
+std::vector<Eigen::Vector3d> PointCloudToEigen(const std::vector<pcl::PointXYZ> & cloud)
+{
+  return cloud | ranges::views::transform(GetXYZ) | ranges::to_vector;
+}
+
+Eigen::Vector3d EstimatePlaneCoefficients(const Eigen::MatrixXd & X)
+{
+  const Eigen::VectorXd g = -1.0 * Eigen::VectorXd::Ones(X.rows());
+  return SolveLinear(X, g);
+}
+
+
 using EdgeSurfaceScan = std::tuple<
   pcl::PointCloud<pcl::PointXYZ>::Ptr,
   pcl::PointCloud<pcl::PointXYZ>::Ptr>;
+
+
+const int n_neighbors = 5;
 
 
 class LOAMOptimizationProblem : public OptimizationProblem<EdgeSurfaceScan>
