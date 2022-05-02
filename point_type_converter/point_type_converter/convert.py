4b9c47449028cc6d735a5d7d5591bcd2bf08c232
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-05-03 02:21:43 +0900

    Add a point type converter package

---
+++ point_type_converter/point_type_converter/convert.py
@@ -0,0 +1,220 @@
+# Copyright 2022 Takeshi Ishita
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the Takeshi Ishita nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import struct
+
+import numpy as np
+
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import PointCloud2, PointField
+
+# PointCloud2 message format.
+
+datatypes = [
+    np.int8, np.uint8,
+    np.int16, np.uint16,
+    np.int32, np.uint32,
+    np.float32, np.float64
+]
+
+format_characters = [
+    'b', 'B',
+    'h', 'H',
+    'i', 'I',
+    'f', 'd'
+]
+
+
+def get_datatype_id(datatype):
+    return datatypes.index(datatype) + 1
+
+
+def get_format_character(datatype_int):
+    return format_characters[datatype_int - 1]
+
+
+def get_datatype(datatype_int):
+    return datatypes[datatype_int - 1]
+
+
+def data_size(datatype):
+    return np.dtype(datatype).itemsize
+
+
+def create_point_format(fields, point_step):
+    format_ = ''
+    index = 0
+    for f in fields:
+        if index < f.offset:
+            format_ += 'x' * (f.offset - index)
+            index = f.offset
+        format_ += get_format_character(f.datatype)
+        index += data_size(get_datatype(f.datatype))
+
+    if index < point_step:
+        format_ += 'x' * (point_step - index)
+    return format_
+
+
+def create_points_format(fields, n_points, point_step, is_bigendian):
+    endian = '>' if is_bigendian else '<'
+    point_format = create_point_format(fields, point_step)
+    return endian + point_format * n_points
+
+
+def unpack_point_bytes(bytes_data, fields, point_step, is_bigendian):
+    assert len(bytes_data) % point_step == 0, \
+           'Data size must be mutiple of point step'
+    n_points = len(bytes_data) // point_step
+    format_ = create_points_format(fields, n_points, point_step, is_bigendian)
+    unpacked = struct.unpack(format_, bytes_data)
+    return split(unpacked, len(fields))
+
+
+def pack_point_data(point_data, fields, point_step, is_bigendian):
+    n_points = len(point_data)
+    format_ = create_points_format(fields, n_points, point_step, is_bigendian)
+    return struct.pack(format_, *flatten(point_data))
+
+
+class BytesToPoint(object):
+
+    def __init__(self, fields):
+        self.names = [f.name for f in fields]
+        self.offsets = [f.offset for f in fields]
+        self.types = [get_datatype(f.datatype) for f in fields]
+        self.sizes = [data_size(datatype) for datatype in self.types]
+
+    def __call__(self, point_byte):
+        def extract(offset, datatype, size):
+            return point_byte[offset:offset+size].view(datatype)[0]
+
+        d = zip(self.names, self.offsets, self.types, self.sizes)
+        return {n: extract(o, t, s) for n, o, t, s in d}
+
+
+def flatten(chunks):
+    return [e for chunk in chunks for e in chunk]
+
+
+def make_point_field(name, offset, datatype, count):
+    return PointField(name=name, offset=offset,
+                      datatype=get_datatype_id(datatype), count=count)
+
+
+def split(collection, chunk_size):
+    n = len(collection)
+    m = chunk_size
+    return tuple(collection[i:i+m] for i in range(n) if i % m == 0)
+
+
+def find_indices(fields, retained_names):
+    return [i for i in range(len(fields)) if fields[i].name in retained_names]
+
+
+def filter_point_data(point_tuples, input_fields, output_fields):
+    output_names = [f.name for f in output_fields]
+    indices = find_indices(input_fields, output_names)
+    return tuple(tuple(chunk[i] for i in indices) for chunk in point_tuples)
+
+
+def make_fields():
+    return [
+        make_point_field('x', 0, np.float32, 1),
+        make_point_field('y', 4, np.float32, 1),
+        make_point_field('z', 8, np.float32, 1),
+        make_point_field('padding', 12, np.float32, 1),
+        make_point_field('intensity', 16, np.float32, 1),
+        make_point_field('ring', 20, np.uint16, 1),
+    ]
+
+
+class PointTypeConverter(Node):
+
+    def __init__(self, *args, **kwargs):
+        super(PointTypeConverter, self).__init__('point_type_converter', *args, **kwargs)
+        self.subscription = self.create_subscription(
+            PointCloud2, '/os1_cloud_node/points', self.callback, 10)
+        self.subscription  # prevent unused variable warning
+        self.publisher = self.create_publisher(PointCloud2, '/points_raw', 10)
+
+    def callback(self, input_cloud):
+        input_fields = input_cloud.fields
+        input_fields.append(make_point_field('padding', 12, np.float32, 1))
+        input_fields = sorted(input_fields, key=lambda f: f.offset)
+        self.get_logger().info('input_fields: {}'.format(input_fields))
+        unpacked = unpack_point_bytes(
+            input_cloud.data, input_fields,
+            input_cloud.point_step, input_cloud.is_bigendian)
+
+        # self.get_logger().info('fields  : {}'.format([f.name for f in fields]))
+        self.get_logger().info('unpacked: {}'.format(unpacked))
+
+        point_step = 32
+
+        output_fields = make_fields()
+        filtered = filter_point_data(unpacked, input_fields, output_fields)
+
+        self.get_logger().info('filtered: {}'.format(filtered))
+
+        data = pack_point_data(filtered, output_fields, point_step, False)
+
+        output_cloud = PointCloud2()
+        output_cloud.header = input_cloud.header
+
+        output_cloud.height = input_cloud.height
+        output_cloud.width = input_cloud.width
+
+        output_cloud.fields = output_fields
+
+        output_cloud.is_bigendian = False
+        output_cloud.point_step = point_step
+        output_cloud.row_step = point_step * input_cloud.width
+        output_cloud.data = data
+        output_cloud.is_dense = input_cloud.is_dense
+
+        self.publisher.publish(output_cloud)
+        # print("point_byte", point_byte)
+        # print(b.view(dtype=struct_type).view(dtype=np.recarray))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    converter = PointTypeConverter()
+
+    rclpy.spin(converter)
+
+    converter.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ point_type_converter/test/test_convert.py
@@ -0,0 +1,256 @@
+# Copyright 2022 Takeshi Ishita
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the Takeshi Ishita nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+
+from point_type_converter.convert import (
+    create_point_format, create_points_format, filter_point_data,
+    pack_point_data, PointTypeConverter, split, unpack_point_bytes)
+
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
+
+from sensor_msgs.msg import PointCloud2, PointField
+
+
+def test_create_point_format():
+    point_step = 48
+    fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1),
+        PointField(name='t', offset=20, datatype=6, count=1),
+        PointField(name='reflectivity', offset=24, datatype=4, count=1),
+        PointField(name='ring', offset=26, datatype=2, count=1),
+        PointField(name='noise', offset=28, datatype=4, count=1),
+        PointField(name='range', offset=32, datatype=6, count=1)
+    ]
+
+    format_ = create_point_format(fields, point_step)
+
+    assert format_ == 'fffxxxxfIHBxHxxIxxxxxxxxxxxx'
+
+
+def test_create_points_format():
+    fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1)
+    ]
+
+    expected = '>fffxxxxfxxxxfffxxxxfxxxx'
+    assert create_points_format(fields, 2, 24, True) == expected
+
+    fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1),
+        PointField(name='ring', offset=20, datatype=2, count=1)
+    ]
+    expected = (
+        '<'
+        'fffxxxxfBxxxxxxxxxxx'
+        'fffxxxxfBxxxxxxxxxxx'
+    )
+    assert create_points_format(fields, 2, 32, False) == expected
+
+
+def test_unpack_point_bytes():
+    data = (
+        b'\x00\x00\x00@\x00\x00\x80@\x00\x00\xc0@\x00\x00\x00\x00\x00\x00 A\x16'
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        b'\x00\x00\x80?\x00\x00@@\x00\x00\xa0@\x00\x00\x00\x00\x00\x00\xa0A\x0b'
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    )
+
+    fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1),
+        PointField(name='ring', offset=20, datatype=2, count=1)
+    ]
+
+    unpacked = unpack_point_bytes(data, fields, 32, False)
+    assert unpacked == ((2., 4., 6., 10., 22), (1., 3., 5., 20., 11))
+
+    fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='padding', offset=12, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1),
+        PointField(name='ring', offset=20, datatype=2, count=1)
+    ]
+
+    unpacked = unpack_point_bytes(data, fields, 32, False)
+    assert unpacked == ((2., 4., 6., 0., 10., 22), (1., 3., 5., 0., 20., 11))
+
+
+def test_pack_point_data():
+    point_data = ((2., 4., 6., 0., 10., 22), (1., 3., 5., 0., 20., 11))
+    fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='padding', offset=12, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1),
+        PointField(name='ring', offset=20, datatype=2, count=1)
+    ]
+
+    packed = pack_point_data(point_data, fields, 32, False)
+    assert unpack_point_bytes(packed, fields, 32, False) == point_data
+
+
+def test_split():
+    collection = (2., 4., 6., 10., 22, 1., 3., 5., 20., 11)
+    chunk_size = 5
+    expected = ((2., 4., 6., 10., 22), (1., 3., 5., 20.,  11))
+    assert split(collection, chunk_size) == expected
+
+
+def test_filter_point_data():
+    input_fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='padding', offset=12, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1),
+        PointField(name='t', offset=20, datatype=6, count=1),
+        PointField(name='reflectivity', offset=24, datatype=4, count=1),
+        PointField(name='ring', offset=26, datatype=2, count=1),
+    ]
+
+    output_fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='padding', offset=12, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1),
+        PointField(name='ring', offset=20, datatype=2, count=1),
+    ]
+
+    point_data = (
+        (1., 2., 3., 0., 40., 60, 100, 6),
+        (4., 6., 8., 0., 30., 80, 200, 7)
+    )
+
+    result = filter_point_data(point_data, input_fields, output_fields)
+
+    expected = (
+        (1., 2., 3., 0., 40., 6),
+        (4., 6., 8., 0., 30., 7)
+    )
+
+    assert result == expected
+
+
+def make_input_cloud():
+    input_cloud = PointCloud2()
+    input_cloud.fields = [
+        PointField(name='x', offset=0, datatype=7, count=1),
+        PointField(name='y', offset=4, datatype=7, count=1),
+        PointField(name='z', offset=8, datatype=7, count=1),
+        PointField(name='intensity', offset=16, datatype=7, count=1),
+        PointField(name='t', offset=20, datatype=6, count=1),
+        PointField(name='reflectivity', offset=24, datatype=4, count=1),
+        PointField(name='ring', offset=26, datatype=2, count=1),
+    ]
+
+    point_data = (
+        (1., 2., 3., 10., 50, 100, 2),
+        (4., 6., 8., 20., 40, 200, 8)
+    )
+    data = pack_point_data(point_data, input_cloud.fields, 32, False)
+    input_cloud.is_bigendian = False
+    input_cloud.is_dense = True
+    input_cloud.height = 1
+    input_cloud.width = 64
+    input_cloud.point_step = 32
+    input_cloud.row_step = 64
+    input_cloud.data = data
+    return input_cloud
+
+
+class TestPointTypeConverter(unittest.TestCase):
+
+    def setUp(self):
+        self.has_called = False
+
+    def check_output_cloud(self, output_cloud):
+        result = unpack_point_bytes(
+            output_cloud.data, output_cloud.fields,
+            output_cloud.point_step, output_cloud.is_bigendian)
+
+        self.assertEqual(
+            result,
+            ((1., 2., 3., 0., 10., 2), (4., 6., 8., 0., 20., 8)))
+
+        # self.assertFalse(output_cloud is None)
+        # self.assertFalse(output_cloud.is_bigendian)
+        # self.assertTrue(output_cloud.is_dense)
+        # self.assertEqual(output_cloud.height, 1)
+        # self.assertEqual(output_cloud.width, 64)
+        # self.assertEqual(output_cloud.point_step, 32)
+        # self.assertEqual(output_cloud.row_step, 32)
+        self.has_called = True
+
+    def test_point_type_converter(self):
+        context = rclpy.context.Context()
+
+        rclpy.init(context=context)
+
+        pub_node = rclpy.create_node('pub_node', context=context)
+        sub_node = rclpy.create_node('sub_node', context=context)
+
+        input_topic = '/os1_cloud_node/points'
+
+        converter = PointTypeConverter(context=context)
+        converter
+
+        pub = pub_node.create_publisher(PointCloud2, input_topic, 10)
+        sub = sub_node.create_subscription(
+            PointCloud2, 'points_raw', self.check_output_cloud, 1)
+        sub
+
+        executor = MultiThreadedExecutor(context=context)
+        executor.add_node(converter)
+        executor.add_node(sub_node)
+
+        for _ in range(5):
+            pub.publish(make_input_cloud())
+            executor.spin_once(timeout_sec=1)
+
+        self.assertTrue(self.has_called)
+
+        pub_node.destroy_node()
+        sub_node.destroy_node()
+        rclpy.shutdown(context=context)
