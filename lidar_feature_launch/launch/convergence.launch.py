99a23f8aa8c581432b1e7dcb543d85313aa8cec5
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-06-29 14:58:12 +0900

    Remove map_loader and map_tf_generator

--- lidar_feature_launch/launch/convergence.launch.py
+++ lidar_feature_launch/launch/convergence.launch.py
@@ -83,31 +83,6 @@ def generate_launch_description():
         ]
     )
 
-    map_loader = Node(
-        package='lidar_feature_map_loader',
-        executable='lidar_feature_map_loader',
-        namespace='lidar_feature_map_loader',
-        remappings=[
-            ('/edge_map', edge_map_topic),
-            ('/surface_map', surface_map_topic),
-        ]
-    )
-
-    map_tf_generator = Node(
-        package='map_tf_generator',
-        executable='map_tf_generator',
-        name='map_tf_generator',
-        parameters=[
-            {
-                'map_frame': 'map',
-                'viewer_frame': 'viewer',
-            }
-        ],
-        remappings=[
-            ('/pointcloud_map', '/edge_map')
-        ]
-    )
-
     path_generator = Node(
         package='path_generator',
         executable='path_generator',
@@ -121,7 +96,5 @@ def generate_launch_description():
     return LaunchDescription([
         extraction,
         convergence,
-        map_loader,
-        map_tf_generator,
         path_generator,
     ])
