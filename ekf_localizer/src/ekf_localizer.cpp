f7eb4abe03538bc78fb0589ed972fd1986c8283c
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-07-21 18:13:15 +0900

    Simplify syntax

--- ekf_localizer/src/ekf_localizer.cpp
+++ ekf_localizer/src/ekf_localizer.cpp
@@ -71,24 +71,39 @@ Eigen::Isometry3d MakePoseFromXYZRPY(
 
 std::array<double, 36> EKFCovarianceToPoseMessageCovariance(const Matrix6d & P)
 {
+  const double p00 = P(0, 0);
+  const double p01 = P(0, 1);
+  const double p02 = P(0, 2);
+  const double p10 = P(1, 0);
+  const double p11 = P(1, 1);
+  const double p12 = P(1, 2);
+  const double p20 = P(2, 0);
+  const double p21 = P(2, 1);
+  const double p22 = P(2, 2);
+
   return std::array<double, 36>{
-    P(0, 0), P(0, 1), 0, 0, 0, P(0, 2),
-    P(1, 0), P(1, 1), 0, 0, 0, P(1, 2),
-          0,       0, 0, 0, 0,       0,
-          0,       0, 0, 0, 0,       0,
-    P(2, 0), P(2, 1), 0, 0, 0, P(2, 2)
+    p00, p01, 0.0, 0.0, 0.0, p02,
+    p10, p11, 0.0, 0.0, 0.0, p12,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    p20, p21, 0.0, 0.0, 0.0, p22
   };
 }
 
 std::array<double, 36> EKFCovarianceToTwistMessageCovariance(const Matrix6d & P)
 {
+  const double p44 = P(4, 4);
+  const double p45 = P(4, 5);
+  const double p54 = P(5, 4);
+  const double p55 = P(5, 5);
+
   return std::array<double, 36>{
-    P(4, 4), 0, 0, 0, 0, P(4, 5),
-          0, 0, 0, 0, 0,       0,
-          0, 0, 0, 0, 0,       0,
-          0, 0, 0, 0, 0,       0,
-          0, 0, 0, 0, 0,       0,
-    P(5, 4), 0, 0, 0, 0, P(5, 5)
+    p44, 0.0, 0.0, 0.0, 0.0, p45,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    p54, 0.0, 0.0, 0.0, 0.0, p55
   };
 }
 
