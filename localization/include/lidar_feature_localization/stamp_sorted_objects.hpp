cf73f5277dc87b090506f83f1a8f1208ebdbe85b
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-10-03 17:47:44 +0900

    Add a class to manage stamp-sorted objects

---
+++ localization/include/lidar_feature_localization/stamp_sorted_objects.hpp
@@ -0,0 +1,103 @@
+// Copyright 2022 Tixiao Shan, Takeshi Ishita
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Tixiao Shan, Takeshi Ishita nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef LIDAR_FEATURE_LOCALIZATION__STAMP_SORTED_OBJECTS_HPP_
+#define LIDAR_FEATURE_LOCALIZATION__STAMP_SORTED_OBJECTS_HPP_
+
+#include <iostream>
+#include <map>
+#include <mutex>
+
+
+template<typename Object>
+class StampSortedObjects
+{
+public:
+  StampSortedObjects()
+  {
+  }
+
+  void Insert(const double timestamp, const Object & object)
+  {
+    objects_[timestamp] = object;
+  }
+
+  Object GetClosest(const double timestamp)
+  {
+    // g1 is the first element in map that satisfies g1 >= timestamp
+    const auto g1 = objects_.lower_bound(timestamp);
+
+    if (g1 == objects_.end()) {
+      const auto last = std::prev(objects_.end());
+      return last->second;
+    }
+
+    if (g1 == objects_.begin()) {
+      return objects_.begin()->second;
+    }
+
+    const auto g0 = std::prev(g1);
+    const auto [time0, object0] = *g0;
+    const auto [time1, object1] = *g1;
+
+    const double d0 = timestamp - time0;
+    const double d1 = time1 - timestamp;
+    return d0 < d1 ? object0 : object1;
+  }
+
+  size_t Size()
+  {
+    return objects_.size();
+  }
+
+  void RemoveOlderThan(const double timestamp)
+  {
+    // g is the first element in map that satisfies timestamp < g
+    const auto g = objects_.upper_bound(timestamp);
+
+    if (g == objects_.end()) {
+      return;
+    }
+
+    const auto boundary = std::prev(g);
+
+    auto curr = std::prev(objects_.end());
+    while (curr != boundary) {
+      auto prev = std::prev(curr);
+      std::cerr << "curr = " << curr->first << " " << curr->second << std::endl;
+      objects_.erase(curr);
+      curr = prev;
+    }
+  }
+
+private:
+  std::mutex mutex_;
+  std::map<double, Object> objects_;
+};
+
+#endif  // LIDAR_FEATURE_LOCALIZATION__STAMP_SORTED_OBJECTS_HPP_
---
+++ localization/test/test_stamp_sorted_objects.cpp
@@ -0,0 +1,74 @@
+// Copyright 2022 Tixiao Shan, Takeshi Ishita
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Tixiao Shan, Takeshi Ishita nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include "lidar_feature_localization/stamp_sorted_objects.hpp"
+
+
+TEST(StampSortedObjects, GetClosest)
+{
+  StampSortedObjects<std::string> q;
+  q.Insert(1.0, std::string{"a"});
+  q.Insert(2.0, std::string{"b"});
+  q.Insert(3.0, std::string{"c"});
+  q.Insert(4.0, std::string{"d"});
+
+  ASSERT_EQ(q.Size(), static_cast<size_t>(4));
+
+  EXPECT_EQ(q.GetClosest(0.0), "a");
+
+  EXPECT_EQ(q.GetClosest(2.5), "c");
+  EXPECT_EQ(q.GetClosest(3.0), "c");
+
+  EXPECT_EQ(q.GetClosest(4.0), "d");
+  EXPECT_EQ(q.GetClosest(5.0), "d");
+}
+
+TEST(StampSortedObjects, RemoveOlderThan)
+{
+  StampSortedObjects<std::string> q;
+  q.Insert(1.0, std::string{"a"});
+  q.Insert(2.0, std::string{"b"});
+  q.Insert(3.0, std::string{"c"});
+  q.Insert(4.0, std::string{"d"});
+
+  q.RemoveOlderThan(4.0);
+
+  ASSERT_EQ(q.Size(), static_cast<size_t>(4));  // nothing removed
+
+  q.RemoveOlderThan(3.0);
+
+  ASSERT_EQ(q.Size(), static_cast<size_t>(3));
+
+  EXPECT_EQ(q.GetClosest(1.0), "a");
+  EXPECT_EQ(q.GetClosest(2.0), "b");
+  EXPECT_EQ(q.GetClosest(3.0), "c");
+}
